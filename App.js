ff4a0096e3a6ab6509c09d23ba2c5b2f021db92b
usri587 <126925703+usri587@users.noreply.github.com>
2024-01-06 13:39:50 +1300

    registration-src

--- App.js
+++ App.js
@@ -1,44 +1,13 @@
 // src/App.js
 
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
-import LoginPage from './Login'
-import AdminPage from './Admin';
-import DashboardPage from './Dashboard'
+import React from 'react';
+import RegistrationForm from './RegistrationForm';
 
 const App = () => {
-  const [loggedInUser, setLoggedInUser] = useState(null);
-  const [users, setUsers] = useState([
-    { username: 'user1' },
-    { username: 'user2' },
-    { username: 'admin' }, // Assume admin user
-  ]);
-
-  const onDeleteUser = (usernameToDelete) => {
-    // For simplicity, let's assume successful deletion
-    setUsers(users.filter((user) => user.username !== usernameToDelete));
-  };
-
-  const ProtectedRoute = ({ component: Component, ...rest }) => (
-    <Route
-      {...rest}
-      render={(props) =>
-        loggedInUser ? <Component {...props} loggedInUser={loggedInUser} /> : <Redirect to="/" />
-      }
-    />
-  );
-
   return (
-    <Router>
-      <Route path="/" exact>
-        <LoginPage setLoggedInUser={setLoggedInUser} />
-      </Route>
-      <ProtectedRoute path="/dashboard" component={DashboardPage} />
-      <ProtectedRoute
-        path="/admin"
-        component={() => <AdminPage users={users} onDeleteUser={onDeleteUser} />}
-      />
-    </Router>
+    <div>
+      <RegistrationForm />
+    </div>
   );
 };
 
